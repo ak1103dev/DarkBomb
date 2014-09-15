06905d2957045ff7a1afb616662ba1ac7a13e95b
Ak1103 <Ak1103@158.108.143.23>
2014-09-15 11:32:18 +0700

    Show Ball at Top-Left

--- src/darkbomb/DarkBombGame.java
+++ src/darkbomb/DarkBombGame.java
@@ -1,7 +1,5 @@
 package darkbomb;
 
-import java.util.ArrayList;
-
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
@@ -12,23 +10,20 @@ public class DarkBombGame extends BasicGame{
 
 	private static final int width = 640;
 	private static final int height = 480;
-	
-	private ArrayList<Entity> entities;
+	private Ball ball;
 	
 	public DarkBombGame(String title) {
 		super(title);
-		setEntities(new ArrayList<Entity>());
 	}
 
 	@Override
 	public void init(GameContainer container) throws SlickException {
-		// TODO Auto-generated method stub
-		
+		ball = new Ball(40, height - 40);
 	}
 	
 	@Override
 	public void render(GameContainer container, Graphics g) throws SlickException {
-		// TODO Auto-generated method stub
+		ball.render();
 		
 	}
 
@@ -49,14 +44,6 @@ public class DarkBombGame extends BasicGame{
 	    catch (SlickException e) {
 	    	e.printStackTrace();
 	    }
-	}
-
-	public ArrayList<Entity> getEntities() {
-		return entities;
-	}
-
-	public void setEntities(ArrayList<Entity> entities) {
-		this.entities = entities;
 	}
 
 }
